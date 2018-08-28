abbaf15a65ec5018c16dca195cc27ff56e71c2e0
Çağatay Çivici <jagatai@Prime-MacBook.local>
2018-08-28 17:47:45 +0300

    Use correct prop name

--- src/components/SampleDemo.js
+++ src/components/SampleDemo.js
@@ -361,7 +361,7 @@ export class SampleDemo extends Component {
                                 <span className="p-messages-icon pi pi-fw pi-2x pi-check"></span>
                                 <ul>
                                     <li>
-                                        <span class="p-messages-detail">Designer API is a theme engine for the complete PrimeReact UI Suite and includes this demo application 
+                                        <span className="p-messages-detail">Designer API is a theme engine for the complete PrimeReact UI Suite and includes this demo application 
                                         to test the commonly used components while designing your theme.
                                         </span>
                                     </li>
