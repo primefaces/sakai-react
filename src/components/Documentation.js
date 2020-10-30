b8b54c63daaa33b566d30d7a3523a8b321a53820
mcandu <dundarmehmetcan@gmail.com>
2020-10-30 12:02:57 +0300

    Updated documatation page

--- src/components/Documentation.js
+++ src/components/Documentation.js
@@ -7,10 +7,10 @@ export const Documentation = () => {
         <div className="p-grid">
             <div className="p-col-12">
                 <div className="card docs">
-                    <h1>Current Version</h1>
-                    <p>React 16.8.6 and PrimeReact 3.1.7</p>
+                    <h4>Current Version</h4>
+                    <p>React 16.8.6 and PrimeReact 5.0.1</p>
 
-                    <h1>Getting Started</h1>
+                    <h4>Getting Started</h4>
                     <p>Sigma is an application template for React based on the popular <a href="https://github.com/facebookincubator/create-react-app">create-react-app</a> that allows
                             creating React apps with no configuration. To get started, clone the <a href="https://github.com/primefaces/sigma-react">repository</a> from GitHub and install the dependencies with npm or yarn.</p>
 <CodeHighlight>
@@ -44,7 +44,7 @@ yarn start
 `}
 </CodeHighlight>
 
-                    <h1>React Scripts</h1>
+                    <h4>React Scripts</h4>
                     <p>Following commands are derived from create-app-app.</p>
 <CodeHighlight>
 {`
@@ -54,142 +54,168 @@ yarn start
 `}
 </CodeHighlight>
 
-                    <h1>Structure</h1>
+                    <h4>Structure</h4>
                     <p>Sigma consists of 2 main parts; the application layout and the resources. <b>App.js</b> inside src folder is the main component containing the template for the base layout
                             whereas required resources such as SASS structure for the layout are placed inside the <b>src/layout</b> folder.</p>
 
-                    <h1>Template</h1>
+                    <h4>Template</h4>
                     <p>Main layout is the JSX template of the App.js, it is divided into a couple of child components such as topbar, profile, menu and footer. Here is render method of the
                     App.js component that implements the logic such as menu state, layout modes and so on.
                         </p>
 
 <CodeHighlight>
 {`
-<div className={wrapperClass} onClick={this.onWrapperClick}>
-    <AppTopbar onToggleMenu={this.onToggleMenu}/>
+<div className={wrapperClass} onClick={onWrapperClick}>
+<AppTopbar onToggleMenu={onToggleMenu} />
 
-    <div ref={(el) => this.sidebar = el} className={sidebarClassName} onClick={this.onSidebarClick}>
+<CSSTransition classNames="layout-sidebar" timeout={{ enter: 200, exit: 200 }} in={isSidebarVisible()} unmountOnExit>
+    <div ref={sidebar} className={sidebarClassName} onClick={onSidebarClick}>
         <div className="layout-logo">
             <img alt="Logo" src={logo} />
         </div>
         <AppProfile />
-        <AppMenu model={this.menu} onMenuItemClick={this.onMenuItemClick} />
+        <AppMenu model={menu} onMenuItemClick={onMenuItemClick} />
     </div>
+</CSSTransition>
 
-    <div className="layout-main">
-        <Route path="/" exact component={Dashboard} />
-        <Route path="/forms" component={FormsDemo} />
-        <Route path="/sample" component={SampleDemo} />
-        <Route path="/data" component={DataDemo} />
-        <Route path="/panels" component={PanelsDemo} />
-        <Route path="/overlays" component={OverlaysDemo} />
-        <Route path="/menus" component={MenusDemo} />
-        <Route path="/messages" component={MessagesDemo} />
-        <Route path="/charts" component={ChartsDemo} />
-        <Route path="/misc" component={MiscDemo} />
-        <Route path="/empty" component={EmptyPage} />
-        <Route path="/documentation" component={Documentation} />
-    </div>
+<AppConfig rippleEffect={ripple} onRippleEffect={onRipple} inputStyle={inputStyle} onInputStyleChange={onInputStyleChange}
+    layoutMode={layoutMode} onLayoutModeChange={onLayoutModeChange} layoutColorMode={layoutColorMode} onColorModeChange={onColorModeChange} />
 
-    <AppFooter />
+<div className="layout-main">
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/formlayout" component={FormLayoutDemo} />
+    <Route path="/input" component={InputDemo} />
+    <Route path="/floatlabel" component={FloatLabelDemo} />
+    <Route path="/button" component={ButtonDemo} />
+    <Route path="/table" component={TableDemo} />
+    <Route path="/list" component={ListDemo} />
+    <Route path="/tree" component={TreeDemo} />
+    <Route path="/panel" component={PanelDemo} />
+    <Route path="/overlay" component={OverlayDemo} />
+    <Route path="/menu" component={MenuDemo} />
+    <Route path="/messages" component={MessageDemo} />
+    <Route path="/file" component={FileDemo} />
+    <Route path="/chart" component={ChartDemo} />
+    <Route path="/misc" component={MiscDemo} />
+    <Route path="/display" component={DisplayDemo} />
+    <Route path="/elevation" component={ElevationDemo} />
+    <Route path="/flexbox" component={FlexBoxDemo} />
+    <Route path="/icons" component={IconsDemo} />
+    <Route path="/grid" component={GridDemo} />
+    <Route path="/spacing" component={SpacingDemo} />
+    <Route path="/typography" component={TypographyDemo} />
+    <Route path="/text" component={TextDemo} />
+    <Route path="/calendar" component={Calendar} />
+    <Route path="/crud" component={Crud} />
+    <Route path="/empty" component={EmptyPage} />
+    <Route path="/documentation" component={Documentation} />
+</div>
 
-    <div className="layout-mask"></div>
+<AppFooter />
+
+<div className="layout-mask"></div>
 </div>
 `}
 </CodeHighlight>
 
-                    <h1>Menu</h1>
+                    <h4>Menu</h4>
                     <p>Menu is a separate component defined in AppMenu.js file based on PrimeReact MenuModel API. In order to define the menuitems,
                     navigate to createMenu() method App.js file and define your own model as a nested structure. Here is the menu component from the demo application.
                             Notice that menu object is bound to the model property of AppMenu component as shown above.</p>
 
 <CodeHighlight>
 {`
-createMenu() {
-    this.menu = [
-        {label: 'Dashboard', icon: 'pi pi-fw pi-home', command: () => {window.location = '#/'}},
-        {
-            label: 'Menu Modes', icon: 'pi pi-fw pi-cog',
-            items: [
-                {label: 'Static Menu', icon: 'pi pi-fw pi-bars',  command: () => this.setState({layoutMode: 'static'}) },
-                {label: 'Overlay Menu', icon: 'pi pi-fw pi-bars',  command: () => this.setState({layoutMode: 'overlay'}) }
-            ]
-        },
-        {
-            label: 'Menu Colors', icon: 'pi pi-fw pi-align-left',
-            items: [
-                {label: 'Dark', icon: 'pi pi-fw pi-bars',  command: () => this.setState({layoutColorMode: 'dark'}) },
-                {label: 'Light', icon: 'pi pi-fw pi-bars',  command: () => this.setState({layoutColorMode: 'light'}) }
-            ]
-        },
-        {
-            label: 'Components', icon: 'pi pi-fw pi-globe', badge: '9',
-            items: [
-                {label: 'Sample Page', icon: 'pi pi-fw pi-star-o', command: () => {window.location = '#/sample'}},
-                {label: 'Forms', icon: 'pi pi-fw pi-calendar', command: () => {window.location = '#/forms'}},
-                {label: 'Data', icon: 'pi pi-fw pi-align-justify', command: () => {window.location = "#/data"}},
-                {label: 'Panels', icon: 'pi pi-fw pi-th-large', command: () => {window.location = "#/panels"}},
-                {label: 'Overlays', icon: 'pi pi-fw pi-clone', command: () => {window.location = "#/overlays"}},
-                {label: 'Menus', icon: 'pi pi-fw pi-bars', command: () => {window.location = "#/menus"}},
-                {label: 'Messages', icon: 'pi pi-fw pi-info-circle', command: () => {window.location = "#/messages"}},
-                {label: 'Charts', icon: 'pi pi-fw pi-clock', command: () => {window.location = "#/charts"}},
-                {label: 'Misc', icon: 'pi pi-fw pi-filter', command: () => {window.location = "#/misc"}}
-            ]
-        },
-        {
-            label: 'Template Pages', icon: 'pi pi-fw pi-file',
-            items: [
-                {label: 'Empty Page', icon: 'pi pi-fw pi-circle-off', command: () => {window.location = "#/empty"}}
-            ]
-        },
-        {
-            label: 'Menu Hierarchy', icon: 'pi pi-fw pi-search',
-            items: [
-                {
-                    label: 'Submenu 1', icon: 'pi pi-fw pi-bookmark',
-                    items: [
-                        {
-                            label: 'Submenu 1.1', icon: 'pi pi-fw pi-bookmark',
-                            items: [
-                                {label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark'},
-                                {label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark'},
-                                {label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark'},
-                            ]
-                        },
-                        {
-                            label: 'Submenu 1.2', icon: 'pi pi-fw pi-bookmark',
-                            items: [
-                                {label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark'},
-                                {label: 'Submenu 1.2.2', icon: 'pi pi-fw pi-bookmark'}
-                            ]
-                        }
-                    ]
-                },
-                {
-                    label: 'Submenu 2', icon: 'pi pi-fw pi-bookmark',
-                    items: [
-                        {
-                            label: 'Submenu 2.1', icon: 'pi pi-fw pi-bookmark',
-                            items: [
-                                {label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark'},
-                                {label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark'},
-                                {label: 'Submenu 2.1.3', icon: 'pi pi-fw pi-bookmark'},
-                            ]
-                        },
-                        {
-                            label: 'Submenu 2.2', icon: 'pi pi-fw pi-bookmark',
-                            items: [
-                                {label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark'},
-                                {label: 'Submenu 2.2.2', icon: 'pi pi-fw pi-bookmark'}
-                            ]
-                        },
-                    ]
-                }
-            ]
-        },
-        {label: 'Documentation', icon: 'pi pi-fw pi-question', command: () => {window.location = "#/documentation"}}
-    ];
-}
+const menu = [
+    { label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' },
+    {
+        label: 'UI Kit', icon: 'pi pi-fw pi-sitemap',
+        items: [
+            { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/formlayout' },
+            { label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/input' },
+            { label: 'Float Label', icon: 'pi pi-fw pi-bookmark', to: '/floatlabel' },
+            { label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/button' },
+            { label: 'Table', icon: 'pi pi-fw pi-table', to: '/table' },
+            { label: 'List', icon: 'pi pi-fw pi-list', to: '/list' },
+            { label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/tree' },
+            { label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/panel' },
+            { label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/overlay' },
+            { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/menu' },
+            { label: 'Message', icon: 'pi pi-fw pi-comment', to: '/messages' },
+            { label: 'File', icon: 'pi pi-fw pi-file', to: '/file' },
+            { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/chart' },
+            { label: 'Misc', icon: 'pi pi-fw pi-circle-off', to: '/misc' },
+        ]
+    },
+    {
+        label: 'Utilities', icon: 'pi pi-fw pi-globe',
+        items: [
+            { label: 'Display', icon: 'pi pi-fw pi-desktop', to: '/display' },
+            { label: 'Elevation', icon: 'pi pi-fw pi-external-link', to: '/elevation' },
+            { label: 'Flexbox', icon: 'pi pi-fw pi-directions', to: '/flexbox' },
+            { label: 'Icons', icon: 'pi pi-fw pi-search', to: '/icons' },
+            { label: 'Grid System', icon: 'pi pi-fw pi-th-large', to: '/grid' },
+            { label: 'Spacing', icon: 'pi pi-fw pi-arrow-right', to: '/spacing' },
+            { label: 'Typography', icon: 'pi pi-fw pi-align-center', to: '/typography' },
+            { label: 'Text', icon: 'pi pi-fw pi-pencil', to: '/text' },
+        ]
+    },
+    {
+        label: 'Pages', icon: 'pi pi-fw pi-clone',
+        items: [
+            { label: 'Crud', icon: 'pi pi-fw pi-user-edit', to: '/crud' },
+            { label: 'Calendar', icon: 'pi pi-fw pi-calendar-plus', to: '/calendar' },
+            { label: 'Empty Page', icon: 'pi pi-fw pi-circle-off', to: '/empty' }
+        ]
+    },
+    {
+        label: 'Menu Hierarchy', icon: 'pi pi-fw pi-search',
+        items: [
+            {
+                label: 'Submenu 1', icon: 'pi pi-fw pi-bookmark',
+                items: [
+                    {
+                        label: 'Submenu 1.1', icon: 'pi pi-fw pi-bookmark',
+                        items: [
+                            { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
+                            { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
+                            { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' },
+                        ]
+                    },
+                    {
+                        label: 'Submenu 1.2', icon: 'pi pi-fw pi-bookmark',
+                        items: [
+                            { label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' },
+                            { label: 'Submenu 1.2.2', icon: 'pi pi-fw pi-bookmark' }
+                        ]
+                    },
+                ]
+            },
+            {
+                label: 'Submenu 2', icon: 'pi pi-fw pi-bookmark',
+                items: [
+                    {
+                        label: 'Submenu 2.1', icon: 'pi pi-fw pi-bookmark',
+                        items: [
+                            { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
+                            { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' },
+                            { label: 'Submenu 2.1.3', icon: 'pi pi-fw pi-bookmark' },
+                        ]
+                    },
+                    {
+                        label: 'Submenu 2.2', icon: 'pi pi-fw pi-bookmark',
+                        items: [
+                            { label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' },
+                            { label: 'Submenu 2.2.2', icon: 'pi pi-fw pi-bookmark' }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    { label: 'Documentation', icon: 'pi pi-fw pi-question', command: () => { window.location = "#/documentation" } },
+    { label: 'View Source', icon: 'pi pi-fw pi-search', command: () => { window.location = "https://github.com/primefaces/sigma-react" } }
+];
+
 `}
 </CodeHighlight>
 
@@ -198,20 +224,20 @@ createMenu() {
 
 <CodeHighlight>
 {`
-"primereact": "^3.1.7",                //required: PrimeReact components
-"primeicons": "1.0.00"                  //required: Icons
+"primereact": "5.0.1",                //required: PrimeReact components
+"primeicons": "4.0.0"                  //required: Icons
 `}
 </CodeHighlight>
 
-                    <h1>Saga Theme</h1>
+                    <h4>Saga Theme</h4>
                     <p>Sigma uses the free Saga-Blue theme of PrimeReact which is a free theme distributed within PrimeReact, however it can be used with any PrimeReact theme.</p>
 
-                    <h1>SASS Variables</h1>
+                    <h4>SASS Variables</h4>
                     <p>In case you'd like to customize the layout variables, open _variables.scss file under src/layout folder. Saving the changes
                     will be reflected instantly at your browser.
                         </p>
 
-                    <h3>src/layout/layout.scss</h3>
+                    <h3>src/layout/_variables.scss</h3>
 <CodeHighlight>
 {`
 /* General */
@@ -223,6 +249,7 @@ $borderRadius:3px;
 $dividerColor:#e3e3e3;
 $transitionDuration:.2s;
 $maskBgColor:#424242;
+$focusShadowColor:#8dcdff;
 
 /* Menu Common */
 $menuitemBadgeBgColor:#007be5;
@@ -263,7 +290,7 @@ $footerBgColor:#ffffff;
 `}
 </CodeHighlight>
 
-                    <h1>Menu Modes</h1>
+                    <h4>Menu Modes</h4>
                     <p>Menu has 2 modes, static and overlay. Main layout container element in App.js is used to define which mode to use by adding specific classes. List
                             below indicates the style classes for each mode.</p>
 
@@ -282,7 +309,7 @@ $footerBgColor:#ffffff;
                     <p>It is also possible to leave the choice to the user by keeping the preference at a component and using an expression to bind it so that user can switch between modes. Sample
                             application has an example implementation of such use case. Refer to App.js for an example.</p>
 
-                    <h1>Menu Color Scheme</h1>
+                    <h4>Menu Color Scheme</h4>
                     <p>There are two alternatives as the menu colors schemes; "light" and "dark". A color scheme is applied using the <i>layout-sidebar-light</i> or <i>layout-sidebar-dark</i>
                         to the sidebar element.</p>
 
@@ -300,11 +327,11 @@ $footerBgColor:#ffffff;
 `}
 </CodeHighlight>
 
-                    <h1>Grid CSS</h1>
+                    <h4>Grid CSS</h4>
                     <p>Sigma uses PrimeFlex CSS Grid throughout the samples. Although any grid library can be used, we recommend using PrimeFlex as your layout framework as it is well tested and supported by PrimeReact. PrimeFlex is
                         available at <a href="https://www.npmjs.com/package/primeflex">NPM</a>.</p>
 
-                    <h1>Customizing Styles</h1>
+                    <h4>Customizing Styles</h4>
                     <p>It is suggested to write your customizations in <i>src/layout/_overrides.scss</i> file instead of adding them to the
                         scss files under sass folder to avoid maintenance issues after an update.</p>
                 </div>
