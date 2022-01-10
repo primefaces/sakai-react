575056284f8f6b03302c5b287ba2f7e0b1d5246d
mcandu <dundarmehmetcan@gmail.com>
2022-01-10 15:21:31 +0300

    Removed sidebar content buttons on OverlayDemo

--- src/components/OverlayDemo.js
+++ src/components/OverlayDemo.js
@@ -135,32 +135,22 @@ export const OverlayDemo = () => {
                         <h5>Sidebar</h5>
                         <Sidebar visible={visibleLeft} onHide={() => setVisibleLeft(false)} baseZIndex={1000}>
                             <h1 style={{ fontWeight: 'normal' }}>Left Sidebar</h1>
-                            <Button type="button" onClick={() => setVisibleLeft(false)} label="Save" className="p-button-success" style={{ marginRight: '.25em' }} />
-                            <Button type="button" onClick={() => setVisibleLeft(false)} label="Cancel" className="p-button-secondary" />
                         </Sidebar>
 
                         <Sidebar visible={visibleRight} onHide={() => setVisibleRight(false)} baseZIndex={1000} position="right">
                             <h1 style={{ fontWeight: 'normal' }}>Right Sidebar</h1>
-                            <Button type="button" onClick={() => setVisibleRight(false)} label="Save" className="p-button-success" style={{ marginRight: '.25em' }} />
-                            <Button type="button" onClick={() => setVisibleRight(false)} label="Cancel" className="p-button-secondary" />
                         </Sidebar>
 
                         <Sidebar visible={visibleTop} onHide={() => setVisibleTop(false)} baseZIndex={1000} position="top">
                             <h1 style={{ fontWeight: 'normal' }}>Top Sidebar</h1>
-                            <Button type="button" onClick={() => setVisibleTop(false)} label="Save" className="p-button-success" style={{ marginRight: '.25em' }} />
-                            <Button type="button" onClick={() => setVisibleTop(false)} label="Cancel" className="p-button-secondary" />
                         </Sidebar>
 
                         <Sidebar visible={visibleBottom} onHide={() => setVisibleBottom(false)} baseZIndex={1000} position="bottom">
                             <h1 style={{ fontWeight: 'normal' }}>Bottom Sidebar</h1>
-                            <Button type="button" onClick={() => setVisibleBottom(false)} label="Save" className="p-button-success" style={{ marginRight: '.25em' }} />
-                            <Button type="button" onClick={() => setVisibleBottom(false)} label="Cancel" className="p-button-secondary" />
                         </Sidebar>
 
                         <Sidebar visible={visibleFullScreen} onHide={() => setVisibleFullScreen(false)} baseZIndex={1000} fullScreen>
                             <h1 style={{ fontWeight: 'normal' }}>Full Screen</h1>
-                            <Button type="button" onClick={() => setVisibleFullScreen(false)} label="Save" className="p-button-success" style={{ marginRight: '.25em' }} />
-                            <Button type="button" onClick={() => setVisibleFullScreen(false)} label="Cancel" className="p-button-secondary" />
                         </Sidebar>
 
                         <Button type="button" icon="pi pi-arrow-right" className="p-button-warning" onClick={() => setVisibleLeft(true)} style={{ marginRight: '.25em' }} />
