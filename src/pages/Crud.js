ed7f7ef03851720660bd75474a2c7dd395fcb6db
mcandu <dundarmehmetcan@gmail.com>
2022-01-12 11:53:58 +0300

    Fixed buttons margin on Crud

--- src/pages/Crud.js
+++ src/pages/Crud.js
@@ -256,7 +256,7 @@ export const Crud = () => {
         return (
             <div className="actions">
                 <Button icon="pi pi-pencil" className="p-button-rounded p-button-success mr-2" onClick={() => editProduct(rowData)} />
-                <Button icon="pi pi-trash" className="p-button-rounded p-button-warning" onClick={() => confirmDeleteProduct(rowData)} />
+                <Button icon="pi pi-trash" className="p-button-rounded p-button-warning mt-2" onClick={() => confirmDeleteProduct(rowData)} />
             </div>
         );
     }
