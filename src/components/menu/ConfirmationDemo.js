56fb772b2ca9a0abecbdf3ec004242a9af3d04d6
Berkay <berkaayyuksel@gmail.com>
2022-08-18 10:28:09 +0300

    test

--- src/components/menu/ConfirmationDemo.js
+++ src/components/menu/ConfirmationDemo.js
@@ -4,7 +4,7 @@ export const ConfirmationDemo = () => {
     return (
         <div className="flex align-items-center py-5 px-3">
             <i className="pi pi-fw pi-check mr-2 text-2xl" />
-            <p className="m-0 text-lg">Confirmation Component Content via Child Route</p>
+            <p className="m-0 text-lg">Confirmation Component Content via Child Route </p>
         </div>
     )
 }
