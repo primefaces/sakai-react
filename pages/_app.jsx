82e7564642f5bd07b1f5c5b12a46134a54c89023
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-10-16 03:27:32 +0000

    set inits

---
+++ pages/_app.jsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import "primereact/resources/primereact.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import "prismjs/themes/prism-dark.css";
+import "../src/assets/demo/flags/flags.css";
+import "../src/assets/demo/Demos.scss";
+import "../src/assets/layout/layout.scss";
+import "../public/assets/themes/lara-light-indigo/theme.css";
+
+export default function MyApp({ Component, pageProps: { ...pageProps } }) {
+    return (
+        <>
+            <Head>
+                <meta charset="utf-8" />
+                <title>PrimeReact - Sakai</title>
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
+}
---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head/>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
