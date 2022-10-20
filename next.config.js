100a39b689144db473554ba46c54457e0d8fd181
Cagatay Civici <cagatay.civici@gmail.com>
2022-10-20 17:22:47 +0300

    Init repo

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: false,
+    trailingSlash: true,
+    basePath: process.env.NODE_ENV === 'production' ? '/sakai-react' : '',
+    publicRuntimeConfig: {
+        contextPath: process.env.NODE_ENV === 'production' ? '/sakai-react' : '',
+        uploadPath: process.env.NODE_ENV === 'production' ? '/sakai-react/upload.php' : '/api/upload'
+    }
+};
+
+module.exports = nextConfig;
