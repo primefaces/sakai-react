68b371158f9f9e293218173d8fd74b3ed90d4d75
Cagatay Civici <cagatay.civici@gmail.com>
2022-10-21 23:08:51 +0300

    Add meta tags

--- layout/layout.js
+++ layout/layout.js
@@ -106,9 +106,17 @@ const Layout = (props) => {
     return (
         <React.Fragment>
             <Head>
-                <base href={contextPath}></base>
-                <title>Sakai React with NextJS</title>
+                <title>Sakai by PrimeReact | Free Admin Template for NextJS</title>
                 <meta charSet="UTF-8" />
+                <meta name="description" content="The ultimate collection of design-agnostic, flexible and accessible React UI Components." />
+                <meta name="robots" content="index, follow" />
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+                <meta property="og:type" content="website"></meta>
+                <meta property="og:title" content="Sakai by PrimeReact | Free Admin Template for NextJS"></meta>
+                <meta property="og:url" content="https://www.primefaces.org/sakai-react"></meta>
+                <meta property="og:description" content="The ultimate collection of design-agnostic, flexible and accessible React UI Components." />
+                <meta property="og:image" content="https://www.primefaces.org/static/social/sakai-nextjs.png"></meta>
+                <meta property="og:ttl" content="604800"></meta>
                 <link rel="icon" href={`${contextPath}/favicon.ico`} type="image/x-icon"></link>
             </Head>
 
