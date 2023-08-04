97a1fe4680d6e6bd31ddc687736b36c3e8bf3458
Burak Sağlam <bsaglam089@gmail.com>
2023-08-04 18:23:22 +0300

    Refactor

--- layout/AppConfig.tsx
+++ layout/AppConfig.tsx
@@ -7,7 +7,7 @@ import { RadioButton, RadioButtonChangeEvent } from 'primereact/radiobutton';
 import { Sidebar } from 'primereact/sidebar';
 import { classNames } from 'primereact/utils';
 import React, { useContext, useEffect, useState } from 'react';
-import { AppConfigProps, LayoutConfig, LayoutState } from '../../types/types';
+import { AppConfigProps, LayoutConfig, LayoutState } from '../types/types';
 import { LayoutContext } from './context/layoutcontext';
 
 const AppConfig = (props: AppConfigProps) => {
