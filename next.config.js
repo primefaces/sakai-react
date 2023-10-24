7adaec2f3e1c0d5d32f8b9b383b57dfb4bb672ee
Nam Nguyen <hoainamcse@gmail.com>
2023-10-24 13:08:14 +0700

    next config: sass options

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  }
+}
 
 module.exports = nextConfig
