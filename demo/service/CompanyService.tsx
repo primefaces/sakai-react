00dbd1babf84a78c1376a6131e9a1baaf3de4064
cenkalp <cenk@bayzat.com>
2023-10-30 21:01:37 +0300

    firestore function added cc.

--- demo/service/CompanyService.tsx
+++ demo/service/CompanyService.tsx
@@ -1,9 +1,10 @@
+import {Company} from "../../types/company";
 
 export const CompanyService = {
     getCompanies() {
         return fetch('/demo/data/companies.json', {headers: {'Cache-Control': 'no-cache'}})
             .then((res) => res.json())
-            .then((d) => d.data as CompanyModule.Company[]);
+            .then((d) => d.data as Company[]);
     },
 
 }
