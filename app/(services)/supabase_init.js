1266907f83284cc145276ce03ccc0b424d157ab7
bngiahuy <45910202+bngiahuy@users.noreply.github.com>
2023-11-01 16:28:00 +0700

    Delete supabase_init.js

--- app/(services)/supabase_init.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://sxzwzgblvfdjwphlmdqx.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
