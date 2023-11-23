54ac9c81812a1b7312e45d0fc33ee05113d3eaf4
bngiahuy <45910202+bngiahuy@users.noreply.github.com>
2023-11-23 15:43:25 +0700

    Create route.js

---
+++ app/api/totrinhdgkk/[slug]/route.js
@@ -0,0 +1,38 @@
+import { supabase } from 'utils/supabaseClient'
+import { NextResponse } from 'next/server'
+
+export async function GET(request, { params }) {
+  const { data, error } = await supabase.from('to_trinh_dgkk').select('*, khach_hang(*)').eq('ma_to_trinh', params.slug)
+
+  if (error) {
+    return NextResponse.json(error, { status: 400 })
+  }
+
+  if (data.length === 0) {
+    return NextResponse.json({ message: 'Không tìm thấy tờ trình' }, { status: 404 })
+  }
+
+  return NextResponse.json(data[0])
+}
+
+export async function PATCH(request, { params }) {
+  const res = await request.json()
+
+  const { error } = await supabase.from('to_trinh_dgkk').update(res).eq('ma_to_trinh', params.slug)
+
+  if (error) {
+    return NextResponse.json(error, { status: 400 })
+  }
+
+  return NextResponse.json({ message: 'Tờ trình đã được cập nhật' })
+}
+
+export async function DELETE(request, { params }) {
+  const { error } = await supabase.from('to_trinh_dgkk').delete().eq('ma_to_trinh', params.slug)
+
+  if (error) {
+    return NextResponse.json(error, { status: 400 })
+  }
+
+  return NextResponse.json({ message: 'Tờ trình đã được xóa' })
+}
