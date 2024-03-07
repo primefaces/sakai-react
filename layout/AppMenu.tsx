47aff79ef012773daffa362d9e154103788d89ad
Toprak Koç <hello@toprak.dev>
2024-03-07 15:20:46 +0300

    Added Figma Link

--- layout/AppMenu.tsx
+++ layout/AppMenu.tsx
@@ -156,6 +156,12 @@ const AppMenu = () => {
                     to: '/documentation'
                 },
                 {
+                    label: 'Figma',
+                    url: 'https://www.dropbox.com/scl/fi/bhfwymnk8wu0g5530ceas/sakai-2023.fig?rlkey=u0c8n6xgn44db9t4zkd1brr3l&dl=0',
+                    icon: 'pi pi-fw pi-pencil',
+                    target: '_blank'
+                },
+                {
                     label: 'View Source',
                     icon: 'pi pi-fw pi-search',
                     url: 'https://github.com/primefaces/sakai-react',
